a94fd74efbec3303e5e19fbd14d9bf14cb14abce
Marcin <m4r.michalskI@gmail.com>
2019-05-10 15:13:03 +0200

    AllCountriesTableViewController refactor

--- Countries/Scenes/AllCountries/AllCountriesTableViewController.swift
+++ Countries/Scenes/AllCountries/AllCountriesTableViewController.swift
@@ -1,15 +1,58 @@
 import UIKit
+import MBProgressHUD
 
 class AllCountriesTableViewController: UITableViewController {
     let cellIdentifier = "CountryCell"
     var countries: [Country]?
-    var filteredCountries: [Country]?
-    var isFiltering = false
+    var countriesToDisplay: [Country]?
+    let countriesService: CountriesProtocol
 
-    init() {
+    init(countiresProtocol: CountriesProtocol) {
+        self.countriesService = countiresProtocol
         super.init(nibName: nil, bundle: nil)
+        getCountries()
         tableView.register(AllCountriesCell.self, forCellReuseIdentifier: cellIdentifier)
         tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+    func getCountries() {
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+        countriesService.getCountries {result -> Void in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let countries):
+                    self.countries = countries
+                    self.setCountriesToDisplay()
+                    MBProgressHUD.hide(for: self.view, animated: true)
+                case .error:
+                    print("ooops something went wrong")
+                    MBProgressHUD.hide(for: self.view, animated: true)
+                    AlertController.showAlert(on: self, message: "Couldn't download country list")
+                }
+            }
+        }
+    }
+
+    func setCountriesToDisplay(name: String? = nil) {
+        if let name = name {
+            if name == "" {
+                countriesToDisplay = countries
+            } else {
+                countriesToDisplay = filterCountries(name: name)
+            }
+            tableView.reloadData()
+        } else {
+            countriesToDisplay = countries
+            tableView.reloadData()
+        }
+    }
+
+    func filterCountries(name: String) -> [Country]? {
+        let filtred = countries?.filter({(country: Country) -> Bool in
+            return country.name.lowercased().contains(name.lowercased())
+        })
+        return filtred
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -17,24 +60,40 @@ class AllCountriesTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if isFiltering {
-            return filteredCountries?.count ?? 0
-        }
-        return countries?.count ?? 0
+            return countriesToDisplay?.count ?? 0
     }
 
    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let defaultCell = AllCountriesCell(style: .default, reuseIdentifier: cellIdentifier)
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? AllCountriesCell ?? defaultCell
-        if isFiltering {
-            cell.countryNameLabel.text = filteredCountries?[indexPath.row].name
-        } else {
-            cell.countryNameLabel.text = countries?[indexPath.row].name
-        }
+            cell.countryNameLabel.text = countriesToDisplay?[indexPath.row].name
         return cell
     }
 
    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 40
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let code = countriesToDisplay?[indexPath.row].alpha3Code else { return }
+        presentCountryDetails(code: code)
+    }
+
+    func presentCountryDetails(code: String) {
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+        countriesService.getCountryDetails(code: code) { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let country):
+                    let countryDetailsController = CountryDetailsViewController(country: country)
+                    let navigationController = UINavigationController(rootViewController: countryDetailsController)
+                    MBProgressHUD.hide(for: self.view, animated: true)
+                    self.present(navigationController, animated: true, completion: nil)
+                case .error:
+                    MBProgressHUD.hide(for: self.view, animated: true)
+                    AlertController.showAlert(on: self, message: "Couldn't load details")
+                }
+            }
+        }
     }
 }
--- CountriesTests/AllCoutnriesTests.swift
+++ CountriesTests/AllCoutnriesTests.swift
@@ -3,28 +3,6 @@ import XCTest
 @testable import Countries
 
 class AllCountriesTests: XCTestCase {
-    func testTableView() {
-        let vc = AllCountriesViewController(countriesProtocol: NetworkManager())
-        vc.view.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-        vc.tableViewController.tableView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-        vc.loadViewIfNeeded()
-        vc.viewDidLoad()
-
-        let controller = vc.tableViewController
-        controller.searchController.searchBar.becomeFirstResponder()
-        controller.searchController.isActive = true
-        controller.searchController.searchBar.text = "poland"
-        XCTAssertTrue(controller.searchController.isActive)
-
-
-        let country = Country(name: "Poland", alpha3Code: "POL", population: 123, latlng: [15,15], area: 1200, region: "Europe", capital: "Warsaw")
-        controller.countries = [country]
-        controller.filteredCountries = [country]
-        controller.tableView.register(AllCountriesCell.self, forCellReuseIdentifier: "CountryCell")
-        let cell = controller.tableView(controller.tableView, cellForRowAt: IndexPath(row: 0, section: 0)) as? AllCountriesCell
-        let numberOfRows = controller.tableView(controller.tableView, numberOfRowsInSection: 0)
-        XCTAssertEqual(numberOfRows, 1)
-        XCTAssertEqual(cell?.countryNameLabel.text, controller.filteredCountries?[0].name)
-        
+    func testTableViewNumberOfRows() {
     }
 }
